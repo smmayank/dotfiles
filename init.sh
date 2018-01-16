ee2b2eb45ba407ba14e7908b5793245c414b626a
Mayank Saxena <sm.mayank@icloud.com>
2018-01-16 15:50:15 +0100

    Adding entry for dotfile in .bashrc

--- init.sh
+++ init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+FORMAT='\033[1;35m'
+RESET='\033[0m' # No Color
+
 # Simple function to create links in home dir
 # params include
 # 1 -> link name
 # 2 -> source path
 create_link() {
-  FORMAT='\033[1;35m'
-  RESET='\033[0m' # No Color
   printf "Creating links for $FORMAT$1$RESET\n"
   rm -rf "$HOME/$1"
   ln -s "$2" "$HOME/$1"
@@ -14,5 +15,20 @@ create_link() {
 
 config_dir="$PWD/configs"
 
+# check if bashrc exist if not create one
+if [ ! -f $HOME/.bashrc ]; then
+  touch $HOME/.bashrc
+fi
+
+# Append config/bashrc to $HOME/.bashrc
+load_bashrc_d=$(echo "source $config_dir/bashrc")
+if ! grep -q "$load_bashrc_d" $HOME/.bashrc; then
+  printf "Adding dotfile entry in $FORMAT~/.bashrc$RESET\n"
+  echo >> $HOME/.bashrc;
+  echo "# adding entry point for dotfiles" >> $HOME/.bashrc;
+  echo $load_bashrc_d >> $HOME/.bashrc;
+fi
+
+# create links for configs
 create_link .vimrc "$config_dir/vimrc"
 create_link .vim "$config_dir/vim"
