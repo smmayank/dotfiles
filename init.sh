a43ab325a5e0a55a3b1216878c06b730a118a18a
Mayank Saxena <sm.mayank@icloud.com>
2018-01-16 13:13:20 +0100

    Dir structire change for redeability

--- init.sh
+++ init.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Simple function to create links in home dir
+# params include
+# 1 -> link name
+# 2 -> source path
 create_link() {
   FORMAT='\033[1;35m'
   RESET='\033[0m' # No Color
   printf "Creating links for $FORMAT$1$RESET\n"
   rm -rf "$HOME/$1"
-  ln -s "$2" "$HOME"
+  ln -s "$2" "$HOME/$1"
 }
 
 config_dir="$PWD/configs"
 
-create_link .vimrc "$config_dir/vim/.vimrc"
-create_link .vim "$config_dir/vim/.vim"
+create_link .vimrc "$config_dir/vimrc"
+create_link .vim "$config_dir/vim"
